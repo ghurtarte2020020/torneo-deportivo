90a17f5998eb600df316b796d4980eca72640aa6
Gabriel Hurtarte <ghurtarte-2020020@kinal.edu.gt>
2022-06-06 09:06:26 -0600

    puerto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const usuarioControlador = require('./src/controllers/usuario.controller');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/ligas', { useNewUrlParser: true, useUnifiedTopology: true}).then(() =>{
     console.log("Se encuentra conectado a la base de datos");
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log("hola mundo, estoy en el puerto 3000!")
     })
 }).catch(error => console.log(error))
